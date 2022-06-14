28bda85d51a0779029daa45014abda58cb1f8a22
Kurt Baux <ktripodibaux@gmail.com>
2022-06-14 16:24:17 -0400

    commit tree b111b6c295a07b6f5748178634b8b3b56f443b2c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    fetchRamen('http://localhost:3000/ramens');
+
+        const form = document.querySelector('#new-ramen')
+        form.addEventListener('submit', function(event) {
+            event.preventDefault()
+            const form = document.querySelector('#new-ramen')
+            const newName = document.querySelector('#new-name').value
+            const newRest = document.querySelector('#new-restaurant').value
+            const newImg = document.querySelector('#new-image').value
+            const newRating = document.querySelector('#new-rating').value
+            const newComment = document.querySelector('#new-comment').value
+            const newRamen = {
+                'name': newName,
+                'restaurant': newRest,
+                'image': newImg,
+                'rating': newRating,
+                'comment': newComment,
+                }
+            appendRamenPreview(newRamen);
+        fetch('http://localhost:3000/ramens', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newRamen)
+    }).then(response => response.json()).then(data => {
+        console.log('Success:', data);
+      }).catch((error) => {
+        console.error('Error:', error);
+      });
+        })
+        
+});
+
+function changeRamenDisplay(object) {
+    // const div = document.createElement('div')
+    const name = document.querySelector('.name')
+    const rest = document.querySelector('.restaurant')
+    const rate = document.querySelector('#rating-display')
+    const comment = document.querySelector('#comment-display')
+    const img = document.querySelector('.detail-image')
+    const id = object.target.id
+    
+    fetch(`http://localhost:3000/ramens/${id}`).then(res => res.json()).then(data => {
+        name.textContent = data.name
+        rest.textContent = data.restaurant
+        rate.textContent = data.rating
+        comment.textContent = data.comment
+        img.src = data.image
+    })
+}
+
+function fetchRamen(url) {
+    fetch(url).then(resp => resp.json()).then(data => {
+        for (let ramen in data) {
+            appendRamenPreview(data[ramen]);
+        }
+    })
+}
+
+function appendRamenPreview(object) {
+    const img = document.createElement('img')
+    img.src = object.image
+    img.id = object.id
+    img.addEventListener('click', function(object) {
+        changeRamenDisplay(object);
+    })
+    document.querySelector('#ramen-menu').append(img)
+}
+
